ce53036e854359b0c52284c8755bd2de3d81f2f8
kateeeeers <katyaereschenko@gmail.com>
2021-10-26 11:07:41 +0300

    Update hello.php

--- LR2/COOKIE/hello.php
+++ LR2/COOKIE/hello.php
@@ -1,16 +1,16 @@
 <?
-if (isset($_GET["email"]) && isset($_GET["pass"]) && $_GET["pass"] !="")
+if (isset($_POST["email"]) && isset($_POST["pass"]) && $_POST["pass"] !=")
 {
-	$user_email=$_GET["email"];
-	$user_pass=$_GET["pass"];
+	$user_email=$_POST["email"];
+	$user_pass=$_POST["pass"];
 	setcookie("email",$user_email,time()+3600);
 	setcookie("pass",$user_pass,time()+3600);
-	header("Location:/Verholetov/login.php");
+	header("Location:/Ereschenko/hello.php");
 }
 
 print_r($_COOKIE);
 ?>
-<form action="/Ereschenko/index.php" method="GET">
+<form action="/Ereschenko/index.php" method="POST">
 <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
 </form>
 <head></head>
